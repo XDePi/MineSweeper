4ad09388fcbda5f50e5b00dd7b308a072188e1d2
Denis Pekutovskiy <tommythegrant@gmail.com>
2021-04-21 21:09:56 +0600

    Add images support

--- src/ru/depi/game/MineSweeper.java
+++ src/ru/depi/game/MineSweeper.java
@@ -2,6 +2,9 @@ package ru.depi.game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.Objects;
 
 /**
  * @author DePi
@@ -10,6 +13,9 @@ import java.awt.*;
 public class MineSweeper extends JFrame {
 
     private JPanel panel;
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
         new MineSweeper();
@@ -21,8 +27,15 @@ public class MineSweeper extends JFrame {
     }
 
     private void initPanel() {
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(500, 500));
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(getImage("bomb"), 0, 0, this);
+                g.drawImage(getImage("num1"), IMAGE_SIZE, 0, this);
+            }
+        };
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
         add(panel);
     }
 
@@ -33,6 +46,12 @@ public class MineSweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+    }
+
+    private Image getImage(String name) {
+        String fileName = "resources/img/" + name.toLowerCase(Locale.ROOT) + ".png";
+        ImageIcon icon = new ImageIcon(fileName);
+        return icon.getImage();
     }
 
 }
